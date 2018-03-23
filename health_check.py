387159469e28703a1cea14dbceac221530530c90
Dave Arter <github@davea.me>
2018-03-23 10:48:01 +0000

    Initial commit

---
+++ health_check.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import sys
+import logging
+from configparser import ConfigParser
+from os.path import expanduser
+from socket import gethostname
+
+from eq3bt import Thermostat
+from bluepy.btle import BTLEException
+
+log = logging.getLogger("health_check")
+
+
+def check_battery_statuses(config):
+    for room, addresses in config['radiators'].items():
+        addresses = addresses.split(",")
+        for addr in addresses:
+            try:
+                thermostat = Thermostat(addr)
+                thermostat.update()
+            except BTLEException:
+                log.error("Couldn't connect to %s %s!", room, addr)
+                continue
+            if thermostat.low_battery:
+                log.warning("Low battery reported by %s %s", room, addr)
+
+
+def main():
+    formatter = "[%(asctime)s] %(name)s %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=formatter)
+    logging.getLogger('eq3bt').setLevel(logging.ERROR)
+
+    hostname = gethostname().split(".")[0]
+    config = ConfigParser()
+    config.read(expanduser("~/.config/radcontrold/{}.ini".format(hostname)))
+    if not config:
+        log.warning("No config for {}, exiting.".format(hostname))
+        return
+
+    check_battery_statuses(config)
+
+if __name__ == '__main__':
+    main()
---
+++ radcontrold.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import sys
+import logging
+from configparser import ConfigParser
+from os.path import expanduser
+
+from eq3bt import Thermostat, Mode
+from bluepy.btle import BTLEException
+from mqttwrapper import run_script
+
+log = logging.getLogger("eq32mqtt")
+
+
+def callback(topic, payload, config):
+    log.debug("%s %s", topic, payload)
+    room = topic.split("/")[2]
+
+    mode = {
+        b'0': Mode.Closed,
+        b'1': Mode.Open,
+    }.get(payload)
+    if mode is None:
+        log.warning("Ignoring invalid payload on %s", topic)
+        return
+
+    addresses = config['radiators'].get(room, "").split(",")
+    if not addresses:
+        log.warning("No EQ3 addresses in config for %s", room)
+        return
+
+    for address in addresses:
+        log.info("Setting %s in %s to %s", address, room, mode)
+        try:
+            Thermostat(address).mode = mode
+        except BTLEException:
+            log.warning("Couldn't set mode %s for %s in %s", mode, address, room)
+
+
+def main():
+    formatter = "[%(asctime)s] %(name)s %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=formatter)
+
+    hostname = gethostname().split(".")[0]
+    config = ConfigParser()
+    config.read(expanduser("~/.config/radcontrold/{}.ini".format(hostname)))
+    if not config:
+        log.warning("No config for {}, exiting.".format(hostname))
+        return
+
+    run_script(callback, broker=config['mqtt']['broker'], topics=['control/radiator/+/active'], config=config)
+
+
+if __name__ == '__main__':
+    main()
+
